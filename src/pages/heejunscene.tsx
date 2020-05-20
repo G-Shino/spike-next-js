98b75cceade50d065344a6ad507e227af87cdc48
lhjrobo <63150314+lhjrobo@users.noreply.github.com>
2020-05-20 15:26:01 +0900

    Update heejunscene.tsx

--- src/pages/heejunscene.tsx
+++ src/pages/heejunscene.tsx
@@ -1,19 +1,17 @@
 import React, { Suspense } from "react";
 import { Canvas } from "react-three-fiber"; //threejsをReactで使うためのモジュール
 import styled from "@emotion/styled";
-//import { Controls } from "../components/orbit"; //orbitControlsのReact-three-fiber実装,これのコピペ＝＞https://qiita.com/Quarter-lab/items/151f06bddea1fc9cf4d7
 import Link from "next/link";
-//import { LoadedObject } from "../components/loadgltf";
 import { Color } from "../constants/Color";
 import dynamic from "next/dynamic";
 const Controls = dynamic(() => import("../components/orbit"), {
   ssr: false,
 });
-
 const LoadedObject = dynamic(() => import("../components/loadgltf"), {
   ssr: false,
 });
 //threeのメインモジュール以外(OrbitControlsなど)を扱う場合は必ずdynamicを用いること
+
 const ChairScene = () => {
   return (
     <ThreePageStyle>
@@ -33,6 +31,9 @@ const ChairScene = () => {
   //Suspenseは必須、3dデータのロードを管理してくれるらしい。上手くいじるとローディング画面も作れる。
   //Canvasで囲まれてるエレメントがthreejsを表示する窓になっている。
 };
+
+//styles---------------------------------------------------------------
+//reactのためのcssと考えてください。
 const ThreePageStyle = styled.div`
   display: grid;
   justify-content:center;
