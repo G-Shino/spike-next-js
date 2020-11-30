cca7b34bb13eca5323073e4ab09d9e6adcaaf550
G-shino <shinogu2929@gmail.com>
2020-11-30 21:33:25 +0900

    material uiの追加

--- youtube-2020-feb-nextjs/src/containers/Homepage.tsx
+++ youtube-2020-feb-nextjs/src/containers/Homepage.tsx
@@ -1,3 +1,13 @@
-export function HomePage() {
-  return <h1>Home Page</h1>
-}
+import Link from "next/link";
+import Button from "@material-ui/core/Button";
+
+export const HomePage = () => {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Link href="/vehicles">
+        <Button>vehicles</Button>
+      </Link>
+    </>
+  );
+};
