4f3016d4f701accd4e2713fe32030ae72e7e58d9
G-shino <shinogu2929@gmail.com>
2020-12-05 01:41:44 +0900

    getStaticProps and getStaticPaths

---
+++ youtube-2020-april-nextjs/src/lib/openDB.ts
@@ -0,0 +1,17 @@
+import { open } from "sqlite";
+import sqlite3 from "sqlite3";
+// import fs from "fs";
+
+export const openDB = async () => {
+  console.log("new connection");
+  // console.log(__dirname);
+  // fs.readdir("./", (err, files) => {
+  //   console.log(files);
+  // });
+
+  // パスは実行したところが基準になるので注意
+  return open({
+    filename: "./mydb.sqlite",
+    driver: sqlite3.Database
+  });
+};
